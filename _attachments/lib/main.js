79a2a1926998f009a82ca911c92ff201a42210a1
pangratz <cmueller.418@gmail.com>
2012-02-19 19:14:13 +0100

    get messages & available days from CouchDB

--- _attachments/lib/main.js
+++ _attachments/lib/main.js
@@ -6,15 +6,51 @@ require('./views');
 IRC.MessagesView.appendTo('#messages');
 IRC.DaysView.appendTo('#days');
 
-IRC.messagesController.addMessage({
-    date: new Date(),
-    user: {
-        name: 'pangratz'
-    },
-    text: 'emberjs rulez!'
-});
+IRC.set('daysController', IRC.DaysController.create());
+IRC.set('messagesController', IRC.MessagesController.create());
 
-IRC.daysController.addDay({
-    date: new Date(),
-    count: 123
+IRC.set('dataSource', Ember.Object.create({
+
+    loadDay: function(day) {
+        IRC.messagesController.clear();
+        var from = day || IRC.createDateTime();
+        var to = from.advance({
+            day: 1
+        });
+        $.couch.db('irc-logs').view('irc/messages', {
+            success: function(data) {
+                if (data && data.rows && data.rows.length > 0) {
+                    data.rows.forEach(function(row) {
+                        IRC.messagesController.addMessage(row.doc);
+                    });
+                }
+            },
+            include_docs: true,
+            reduce: false,
+            startkey: IRC.getDateArray(from, 'year', 'month', 'day'),
+            endkey: IRC.getDateArray(to, 'year', 'month', 'day')
+        });
+    }
+
+}));
+IRC.dataSource.loadDay(IRC.createDateTime());
+
+$.couch.db('irc-logs').view('irc/messages', {
+    success: function(data) {
+        if (data && data.rows && data.rows.length > 0) {
+            data.rows.forEach(function(doc) {
+                var key = doc.key;
+                var date = Ember.DateTime.create({
+                    year: key[0],
+                    month: key[1],
+                    day: key[2]
+                });
+                IRC.daysController.addDay({
+                    date: date,
+                    count: doc.value
+                });
+            });
+        }
+    },
+    group_level: 3
 });
